e52598571b314e31460db0ec3c3c48f46e25ecb2
dong <1011284295@qq.com>
2019-07-29 11:06:16 +0800

    强制重载方法选择 (#36)
    强制重载方法选择 (#36)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,8 +3,9 @@ package com.github.hcsp.objectbasic;
 public class Main {
     public static void main(String[] args) {
         // 现在被选中的方法是print(int)，请强制令其选中方法print(Number i)
-        print(1);
+        print((Number) 1);
     }
+
 
     public static void print(int i) {
         System.out.println("I'm int!");
