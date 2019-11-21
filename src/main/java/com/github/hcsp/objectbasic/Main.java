b46204732d96001916532661e72c387ec30ca1ee
wgd <872440899@qq.com>
2019-11-21 23:28:48 +0800

    调用特定方法 (#87)
    调用特定方法 (#87)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,7 +3,27 @@ package com.github.hcsp.objectbasic;
 public class Main {
     public static void main(String[] args) {
         // 现在被选中的方法是print(int)，请强制令其选中方法print(Number i)
-        print(1);
+        print(new Number() {
+            @Override
+            public int intValue() {
+                return 1;
+            }
+
+            @Override
+            public long longValue() {
+                return 1;
+            }
+
+            @Override
+            public float floatValue() {
+                return 1;
+            }
+
+            @Override
+            public double doubleValue() {
+                return 1;
+            }
+        });
     }
 
     public static void print(int i) {
