a77c72300a4a17451cd7f47aa732cf811136b7d0
87244099 <872440899@qq.com>
2019-11-22 00:42:21 +0800

    调用特定方法

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,27 +3,7 @@ package com.github.hcsp.objectbasic;
 public class Main {
     public static void main(String[] args) {
         // 现在被选中的方法是print(int)，请强制令其选中方法print(Number i)
-        print(new Number() {
-            @Override
-            public int intValue() {
-                return 1;
-            }
-
-            @Override
-            public long longValue() {
-                return 1;
-            }
-
-            @Override
-            public float floatValue() {
-                return 1;
-            }
-
-            @Override
-            public double doubleValue() {
-                return 1;
-            }
-        });
+        print((Number)1);
     }
 
     public static void print(int i) {
